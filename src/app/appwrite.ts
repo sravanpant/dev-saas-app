a693d4e4907989ed945b89b1a32c959dcad10742
sravanpant <sravan.pant5@gmail.com>
2025-03-24 12:03:00 +0530

    Completed the database setup and tested the queries

--- src/app/appwrite.ts
+++ src/app/appwrite.ts
@@ -1,4 +1,4 @@
-import { Client, Account, OAuthProvider } from "appwrite";
+import { Client, Account, Databases, OAuthProvider } from "appwrite";
 
 export const client = new Client();
 
@@ -7,4 +7,13 @@ client
   .setProject("67e0196f0027ded33a38"); // Replace with your project ID
 
 export const account = new Account(client);
+const databases = new Databases(client);
+
+export const result = await databases.listDocuments(
+  "67e05391003e3e1239e6",
+  "67e053ad00215ec7c6ae"
+);
+
+// console.log(result);
+
 export { OAuthProvider };
